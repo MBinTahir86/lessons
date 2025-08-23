387b67d55375a6e9a695f281ab9cc05c7376c600
tahir-nav1986 <mtah@ciklum.com>
2025-08-23 19:34:17 +0500

    star

---
+++ star.py
@@ -0,0 +1,24 @@
+import turtle
+turtle.Screen().bgcolor("green")
+
+turtle.Screen().setup(800,900)
+triangle=turtle.Turtle()
+triangle.forward(100)
+triangle.left(120)
+triangle.forward(100)
+triangle.left(120)
+triangle.forward(100)
+triangle.penup()
+triangle.right(150)
+triangle.forward(50)
+
+
+triangle.pendown()
+
+triangle.right(90)
+triangle.forward(100)
+triangle.right(120)
+triangle.forward(100)
+triangle.right(120)
+triangle.forward(100)
+turtle.done()
