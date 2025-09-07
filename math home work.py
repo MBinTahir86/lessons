bb59c2884ba56298fa886a03fd53c65eab352b36
tahir-nav1986 <mtah@ciklum.com>
2025-09-07 19:25:20 +0500

    kj

---
+++ math home work.py
@@ -0,0 +1,13 @@
+import math
+
+
+print("2 to the power 3 is", math.pow(2, 3))
+
+
+print("Floor of 7.8 is", math.floor(7.8))
+
+
+print("Ceil of 7.1 is", math.ceil(7.1))
+
+
+print("Factorial of 5 is", math.factorial(5))
