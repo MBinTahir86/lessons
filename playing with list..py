d04b56c316e290a5da722b1a2a3cc3b23d038e3e
tahir-nav1986 <mtah@ciklum.com>
2025-09-19 20:52:31 +0500

    lists

---
+++ playing with list..py
@@ -0,0 +1,9 @@
+list=[132,56,45,34,23]
+print (list)
+print ("this is the original list")
+count=0
+for i in list:
+    count=count+1
+avrg=count/len(list)
+print ("the count is",count)
+print ("the average is",avrg)
