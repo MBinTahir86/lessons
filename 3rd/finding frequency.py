5e0368dbbfe33c4aca12ddeda5de6a9cd32fb700
tahir-nav1986 <mtah@ciklum.com>
2025-10-05 19:22:36 +0500

    frequency

---
+++ 3rd/finding frequency.py
@@ -0,0 +1,7 @@
+codingal = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+var=2
+result=0
+for key in codingal:
+   if codingal [key]==var:
+      result=result + 1
+      print (result)
