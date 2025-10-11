b1d0680b35845d1d3817e23f0d00869f4f43d03e
tahir-nav1986 <mtah@ciklum.com>
2025-10-11 19:14:55 +0500

    dfd

---
+++ array_operations.py
@@ -0,0 +1,8 @@
+import array as arr
+a=arr.array('i',[1,2,4,7,3,3,3,])
+print(a)
+a.count(3)
+print(a.count(3))
+b=arr.array('i',[1,2,4,7])
+b.reverse()
+print(b)        
---
+++ set intersection.py
@@ -0,0 +1,8 @@
+set_1={"black","red"}
+print(set_1)
+set_2={"white","red"}
+print(set_2)
+set__1=set_1.intersection (set_2)
+print(set__1)
+set_3=set_1.union(set_2)
+print(set_3)
