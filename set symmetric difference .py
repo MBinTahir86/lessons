e5681e0b1836c19ea2e0752fcda4eb330481e047
tahir-nav1986 <mtah@ciklum.com>
2025-10-11 19:26:24 +0500

    afttrd564e65

---
+++ set symmetric difference .py
@@ -0,0 +1,14 @@
+def symmetric_difference(set1, set2):
+    return set1.symmetric_difference(set2)
+
+
+A = {1, 2, 3}
+B = {3, 4, 5}
+
+
+result = symmetric_difference(A, B)
+
+
+print("Set A:", A)
+print("Set B:", B)
+print("Symmetric Difference:", result)
